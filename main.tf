8fb17da7ae91bc6fecd717c8b182c025369e2a70
aleks <grigorenko@hpe.com>
2024-05-23 16:07:01 +0300

    add provider

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "example" {
   name     = "${random_pet.prefix.id}-rg"
   location = var.location
